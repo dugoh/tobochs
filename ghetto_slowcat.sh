65d99d2325d63c54bcb631273beb47d24f3f19d2
dugoh <dugo@xs4all.nl>
2016-07-22 16:16:32 +0200

    try

--- ghetto_slowcat.sh
+++
@@ -1,8 +0,0 @@
-[[ -z "${3}" ]] && echo usage: $0 file chunksize waittime && exit
-c=0
-b=$(wc -c <${1})
-  while [ ${c} -lt ${b} ]; do
-  dd if=${1} bs=1 count=${2} skip=${c} 2>/dev/null
-  (( c = c + ${2} ))
-  sleep ${3}
-done
